1717c72f1a6258752f210a5de3a907a785c41c22
nicoladaniello2019-star <nicola.daniello2019@gmail.com>
2026-04-22 17:08:29 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,10 +3,16 @@ import streamlit as st
 st.title("Quiz")
 st.write("Dedico tale quiz al mio amore Alessandro")
 
-
 st.subheader("Domanda 1")
-r1= st.selectbox:
-    ("Quale stato fu fondato dal Padre Fondatore Alessandro Picciau 1209?",
-    ["Macedonia del nord", "Macedonia del sud", " Macedonia centrale"])
+r1 = st.selectbox(
+    "Quale stato fu fondato dal Padre Fondatore Alessandro Picciau nel 1209?",
+    ["Macedonia del nord", "Macedonia del sud", "Macedonia centrale"]
+)
 
+
+if st.button("Verifica"):
+    if r1 == "Macedonia del sud": 
+        st.success("Corretto!")
+    else:
+        st.error("Sbagliato, riprova!")
 
