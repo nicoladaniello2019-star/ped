2471a32686421c3e8e6192556921287f7e167d9b
nicoladaniello2019-star <nicola.daniello2019@gmail.com>
2026-04-22 17:04:54 +0200

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Quiz")
+st.write("Dedico tale quiz al mio amore Alessandro")
+
+
+st.subheader("Domanda 1")
+r1= st.selectbox 
+    ("Quale stato fu fondato dal Padre Fondatore Alessandro Picciau 1209?",
+    ["Macedonia del nord", "Macedonia del sud", " Macedonia centrale"])
+
+
