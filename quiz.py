394d10eb3244b3be94c591cbeea6534d633112be
nicoladaniello2019-star <nicola.daniello2019@gmail.com>
2026-04-22 17:06:36 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -5,7 +5,7 @@ st.write("Dedico tale quiz al mio amore Alessandro")
 
 
 st.subheader("Domanda 1")
-r1= st.selectbox 
+r1= st.selectbox:
     ("Quale stato fu fondato dal Padre Fondatore Alessandro Picciau 1209?",
     ["Macedonia del nord", "Macedonia del sud", " Macedonia centrale"])
 
