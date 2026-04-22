1d87e111ffc48d2258d24a24d46e5708cbd1778a
nicoladaniello2019-star <nicola.daniello2019@gmail.com>
2026-04-22 17:14:56 +0200

    Change radio button to select box for question 1

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ st.title("Quiz")
 st.write("Dedico tale quiz al mio amore Alessandro")
 
 st.subheader("Domanda 1")
-r1 = st.radio(
+r1 = st.selectbox(
     "Quale stato fu fondato dal Padre Fondatore Alessandro Picciau nel 1209?",
     ["Macedonia del nord", "Macedonia del sud", "Macedonia centrale"]
 )
